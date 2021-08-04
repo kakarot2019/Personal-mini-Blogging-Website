eff29b76795dc5908ab889836a6f5e679d3c7b3b
Aditya kumar <Kumadi2018@users.noreply.github.com>
2021-08-05 04:25:51 +0530

    completed website

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var lodash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,9 +16,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+let posts=[]; var flag=0;
 app.get("/", function(req,res){
-  res.render("home", {homeStartingContent: homeStartingContent});
+  res.render("home", {homeStartingContent: homeStartingContent,posts : posts});
+  
 });
 app.get("/about", function(req,res){
   res.render("about", {aboutContent: aboutContent});
@@ -28,10 +30,20 @@ app.get("/contact", function(req,res){
 app.get("/compose", function(req,res){
   res.render("compose");
 });
+app.get("/posts/:something", function(req,res){
+  posts.forEach(function(post){
+    if(lodash.lowerCase(post.title) === lodash.lowerCase(req.params.something))
+      res.render("post" , post);
+  });
+})
 
 app.post("/compose" , function(req,res){
-  note = req.body.note;
-  console.log(note);
+  const post = {
+    title : req.body.title,
+    description : req.body.description,
+  };
+  posts.push(post);
+  res.redirect("/");
 })
 
 
